0e6b649112267873eef598096dd219479a7d7c26
DrInnovatio <turing1723@gmail.com>
2021-11-23 12:32:11 +0900

    allocating the number of array.

--- src/unit_2/Prac_a2.java
+++ src/unit_2/Prac_a2.java
@@ -3,6 +3,7 @@ package unit_2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.Buffer;
 import java.util.Scanner;
 
 
@@ -23,7 +24,49 @@ public class Prac_a2 {
         for(int i=0; i<x.length; i++){
             System.out.println(i + " : " + x[i]);
         }
+
+
+        int[] x = new int[]{3,4,5,6,7,8};
+        System.out.println(x.length);
+        
+        for(int i = 0; i<x.length; i++){
+            System.out.print(x[i]);
+        }
+
+
+        int[] x = new int[5];
+        boolean[]y = new boolean[3];
+
+        for(int i=0; i<5; i++){
+            System.out.println("x[" + i + "] = "  + x[i]);
+        }
+
+        for(int i=0; i<3; i++){
+            System.out.println("y[" + i + "] = "  + y[i]);
+        }
+
          **/
+
+        BufferedReader enter = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = 0;
+        int[] x = null;
+
+        System.out.println("Num = ");
+        num = Integer.parseInt(enter.readLine());
+
+        x = new int[num];
+
+        for(int i=0; i<x.length; i++){
+            System.out.println("x[" + i + "] = "+ x[i]);
+        }
+
+
+
+
+
+
+
 
 
 
